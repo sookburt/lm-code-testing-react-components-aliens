b709cd3101934fe1c00c189346013e06cd146b28
Sue Burt <sookburt@gmail.com>
2022-04-30 19:01:50 +0100

    tests for SelectInput component with interactivity done

--- src/components/SelectInput.test.tsx
+++ src/components/SelectInput.test.tsx
@@ -1,18 +1,49 @@
+import React from 'react';
+import '@testing-library/jest-dom';
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { InputProps } from '../interfaces/InputInterface';
 import SelectInput from './SelectInput';
 
 describe("Select Input", () => {
 
-  test("That Select Label appears on page.", () => {
-    const state: InputProps = {id: 'math_input', labelName: 'What is 2 + 2', value: '0', updateMethod: () => {} }
+  const renderInput = (state:InputProps) => {
+
     render(<SelectInput 
       id={state.id} 
       labelName={state.labelName} 
       value={state.value} 
       updateMethod={state.updateMethod} />);
-    const labelText = screen.getByText(/What is 2 \+ 2/i);
-    expect(labelText).toBeInTheDocument();
+  }
+
+  test("That appears and behaves correctly.", async() => {
+
+    const mock = jest.fn();
+
+    const state: InputProps = {
+      id: 'math_input', 
+      labelName: 'What is 2 + 2?', 
+      value: '0', 
+      updateMethod: mock 
+    }
+    renderInput(state);
+
+    const tag = screen.getByText(/What is 2 \+ 2/i);
+    expect(tag).toBeInTheDocument();
+    
+    const value = screen.getByDisplayValue('Select...');
+    expect(value).toBeInTheDocument();
+
+    expect(screen.getByRole('combobox')).toHaveLength(3)
+
+    await userEvent.selectOptions(
+        screen.getByRole('combobox'), 
+        screen.getByRole('option', { name: 'Not 4'}),
+      );
+    
+    expect((screen.getByRole('option', { name: /Not 4/i }) as HTMLOptionElement).selected).toBe(true); // failing
+    expect(mock).toHaveBeenCalled();
+
   });
 
 });
